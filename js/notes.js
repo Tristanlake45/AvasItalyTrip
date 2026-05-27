2aea0cf8c6cb2fb7a570939938d224523a059da9
Tristanlake45 <tristan.lake@southeasttech.edu>
2026-05-26 20:33:33 -0500

    Add week two notes

--- js/notes.js
+++ js/notes.js
@@ -32,5 +32,75 @@ const notes = [
   {
     day: 7,
     message: `HAPPY 1 WEEK IN ITALY! I’m so proud of you completing a full ass week in a completely different nation. I hope by now you have been able to experience all sorts of cultural staples! So, I’m brining back the challenge: let me know you’re favorite piece of art, food, and place you’ve seen/had so far! I am so excited to talk to you again and look forward to hearing your voice more than you could imagine! You’re truly my best friend and the love of my life, which makes this being away shit pretty hard, but we’re getting closer everyday! I love you!`
+  },
+
+  {
+    day: 8,
+    message: `HELLO BABE! Happy start of week 2! I hope you had a great night sleep and are fired up to look at some paintings and stuff for your class! I miss you real bad both at the time of writing this and you reading it. I’m so glad that week 1 is already done because it means I’m a week closer to seeing you. I want to challenge you to take a picture of a crazy looking painting that you see while out and about! I can’t wait till you’re finally home and we can wake up at the same time again: (I never knew being in different time zones could be so hard) I love you babe and I am YEARNING to see you! 
+Yearning is fun to say so I’m gonna make it the word of the day!
+
+Word of the day:
+yearning
+noun (https://www.merriam-webster.com/dictionary/noun)
+yearn· ing ˈyər-niŋ  (https://www.merriam-webster.com/dictionary/yearning?pronunciation&lang=en_us&dir=y&file=yearn01v)
+Synonyms of yearning (https://www.merriam-webster.com/thesaurus/yearning)
+  : a tender or urgent longing`
+  },
+
+  {
+    day: 9,
+    message: `Hello baby! I hope you’re having a good day so far whatever time of day it is you’re reading this! Since I’m not sure when you’ll read it, I’ll make a message suitable for all hours…
+
+Morning: MORNING SUNSHINE, I hope you had a great night sleep! Have a fantastic day my love, and make sure to eat a banging breakfast! Because, as according to Gibney and other researchers: “Today, there would appear to be universal recognition that breakfast should play a significant role in helping consumers attain an optimal nutritional profile” (Gibney et al.). MLA format for you my love!
+
+Afternoon: hellooooo baby! Happy afternoon! I hope the weather is nice and you had a banging lunch! Because as according to…nevermind that’s too much research. I hope by this time in the day you’re done with class and that it went well, I’m missing you so much and I can’t wait to talk to you when I wake up! I love you to the moon and back, talk soon!
+
+Nighttime: hello my love! I hope by now you have had a great day and are winding down for bed! I just want to let you know that I spent all day missing you and daydreaming of being with you again. I hope you get a great night sleep, and I cannot wait to talk to you tomorrow afternoon!`
+  },
+
+  {
+    day: 10,
+    message: `hellllllo babeh! Happy day 10! Since its day 10, I wanted to give you a list of the top 10 things I’ve missed most since you’ve been gone:
+#1: you’re adorable laugh when it’s just us together and we’re acting odd
+
+#2: how you smile when I compliment you/how you light up when I do something nice or cute
+#3: how comfortable you make me feel being myself
+#4: the way we bring out our inner child when we’re together
+#5: how much fun it is talking about our future together
+#6: driving places together, no matter the distance
+#7: our amazing date nights
+#8: watching you get ready and put on makeup in person
+#9: watching a fun show together
+#10: being able to hold your hand and have you near me anytime we’re together`
+  },
+
+  {
+    day: 11,
+    message: `hello again! Today I wanted to try something out, so I’m going to ask a couple questions and I would love for you to send me videos/voice notes with the answers!
+
+1: what has been your favorite city so far
+
+2: would you ever see yourself (aka us) living in any of these countries? (Or maybe just visiting?)
+
+3: what weird difference from America have you liked most about Italy?
+
+4: what weird difference do you think I would like most?
+
+I look forward to hearing your answers baby! I hope you have/had a great day, and I love you so much. I can’t wait to build our future together and take a trip back to Italy someday day!`
+  },
+
+  {
+    day: 12,
+    message: `hello babe! Happy day 12! We’re almost at the two week mark breh!! I hope you’re having a great day in ita-lay! Today I just want to let you know how happy and lucky I am to have you in my life. You know me better than anyone else, and you complete me in a way nothing else can. While you being away is really hard, I just know when you come back we are going to have the best rest of our summer. I can’t wait for nights at the lake with you, and our summer date nights together. I look forward to being able to give you a fat hug once you return! I love you and I miss you more than you could imagine.`
+  },
+
+  {
+    day: 13,
+    message: `hi again babe! Just wanting to write you this letter to let you know how excited I am for you to be on this fun adventure in Italy. Some days I find myself being unnecessarily grumpy towards you, and I’m so sorry for it. Being away from you is hard, especially when you’re doing so much awesome stuff. There’s nothing I want more than to be with you, so the distance hits me real hard. I just want you to know even when my attitude gets the best of me, I am still so excited and PROUD of you for studying abroad and getting to have such a fun once in a lifetime experience. I know being away from home is not easy, but I just want you to know I’m always a text/call away. I love you sweetheart, and I will always be there for you, always!`
+  },
+
+  {
+    day: 14,
+    message: `HAPPY END OF WEEK TWO! We are getting another week closer to your long awaited return! I hope you have had a great time so far on your trip! I also hope that you have learned and grown a lot while soaking in the culture and sunshine of the big boot. I can’t wait for you to return and unload all your cool stories and adventures on me, perhaps over some chips and queso! I love you so much, and being this far from you has definitely not been easy, but I know when you’re back in my arms it’ll all be alright! Cheers to week 3, ciao (that’s Italian for peace out)`
   }
 ];
